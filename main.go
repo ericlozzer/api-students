891ef14f96fc96f60574f0a9406b8c2682cad88b
Eric Lozzer <ericlozzer@yahoo.com.br>
2025-03-14 17:05:50 -0300

    Get students in DB

--- main.go
+++ main.go
@@ -34,7 +34,12 @@ func main() {
 
 // Handler
 func getStudents(c echo.Context) error {
-	return c.String(http.StatusOK, "List of all students")
+	students, err := db.GetStudents()
+	if err != nil {
+		return c.String(http.StatusNotFound, "Failed to get students")
+	}
+
+	return c.JSON(http.StatusOK, students)
 }
 
 func createStudent(c echo.Context) error {
@@ -45,7 +50,7 @@ func createStudent(c echo.Context) error {
 	if err := db.AddStudent(student); err != nil {
 		return c.String(http.StatusInternalServerError, "Error to create student")
 	}
-	
+
 	return c.String(http.StatusOK, "Create student")
 }
 
